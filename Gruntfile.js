ac75ab50e63bc1dc3275795aa7c2fce4d8841634
Victor <victor.xiong24@gmail.com>
2016-01-16 19:30:04 -0500

    add npm and bower packages

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pkgFile: 'package.json',
+		eslint: {
+			target: [
+				'lib/*.js',
+				'src/*.js',
+				'test/*.js',
+				'Gruntfile.js'
+			]
+		},
+		watch: {
+			files: ['**/*.js'],
+			tasks: ['eslint']
+		}
+	});
+
+	grunt.registerTask('default', ['eslint']);
+
+	require('load-grunt-tasks')(grunt);
+	
+};

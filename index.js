cce7f71f1ce6f1f6ea0dedbad9b41ae452544351
martinsuchen35 <martinsuchen3557@gmail.com>
2016-02-28 02:15:15 -0500

    Refactor index.js.

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-'use strict';
+var express = require('express')
+var app = express()
 
-var express = require('express');
-var app = express();
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/app'))
 
-app.use(express.static(__dirname + '/app'));
+app.get('/', function(request, response) {
+    response.send('Hello World!')
+})
 
-app.listen(8080);
-console.log("App listening on port 8080");
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'))
+})

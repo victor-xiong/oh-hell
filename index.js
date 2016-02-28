76d93e644cfaaba01e1f1053d94e760387f66f9d
martinsuchen35 <martinsuchen3557@gmail.com>
2016-02-28 02:05:22 -0500

    Add NodeJS bootstrap file.

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/app'));
+
+app.listen(8080);
+console.log("App listening on port 8080");
